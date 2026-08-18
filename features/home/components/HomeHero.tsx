1660392b56369aff23269237db7dc039a1c9e68a
hdyui <tranvuhaiduy2006hg@gmail.com>
2026-08-18 12:21:41 +0700

    update hero

--- features/home/components/HomeHero.tsx
+++ features/home/components/HomeHero.tsx
@@ -167,7 +167,7 @@ export function HomeHero() {
                 fill
                 priority={index === 0}
                 sizes="100vw"
-                quality={40}
+                quality={20}
                 // unoptimized
               />
             </div>
